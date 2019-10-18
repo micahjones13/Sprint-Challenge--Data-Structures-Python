ec0d0633352b7fa11b8c190c5ec9e7e5310fd841
micahjones13 <micahjones13@gmail.com>
2019-10-18 10:46:37 -0600

    acutally pushing it all up now oops

--- names/names.py
+++ names/names.py
@@ -11,12 +11,95 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+# 1.5 sec
+# for name in names_1:
+#     if name in names_2:
+#         duplicates.append(name)
+
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree | key and value are same in these.
+    def insert(self, value):
+        if value < self.value:
+            # go left
+            # if there is nothing to the left
+            if not self.left:
+                # insert it. It's a binary search tree.
+                self.left = BinarySearchTree(value)
+            # if there is something there, keep going until there isn't!
+            else:
+                self.left.insert(value)
+        else:
+            # go right
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+
+        if target < self.value:
+            # go left
+            if not self.left:
+                # it's not here
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            # target > value, go right
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        # just keep going right until you can't, then return that value
+        if not self.right:
+            return self.value
+        return self.right.get_max()
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    # just modifying values, so no need to return the recursion call
+    def for_each(self, cb):
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
+
+# .1 sec
+tree = BinarySearchTree('a')
+for name_1 in names_1:
+    tree.insert(name_1)
+for name_2 in names_2:
+    if tree.contains(name_2):
+        duplicates.append(name_2)
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+
+"""
+#just storing orignal 
+
+duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+"""
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,39 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        # append item to storage at current place
+        # if self.capacity == self.current:
+        #     self.current = 0
+        # self.storage.append(item)
+        # del self.storage[0:2]
 
-  def get(self):
-    pass
+        # if the capicty == current, restart the counter and overwrite the older values
+        if self.capacity == self.current:
+            self.current = 0
+        # since it's a pre-allocated list, just need to change the value at each index of the list
+        # using current as a counter, we go through each place in the list and update it until we reach capacity
+        self.storage[self.current] = item
+        # up counter
+        self.current += 1
+
+    def get(self):
+        new = []
+        # don't want to return None values in the list.
+        for i in range(len(self.storage)):
+            if self.storage[i] != None:
+                new.append(self.storage[i])
+
+        return new
+
+
+test = RingBuffer(5)
+
+test.append('a')
+test.append('b')
+test.append('c')
+test.append('d')
+print(test.get())
